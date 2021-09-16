583dfa670a4210c172c6a12b3fb61f4d505d42e2
Carl148Carl148 <Carl148Carl148@users.noreply.github.com>
2021-09-15 21:12:30 -0700

    First Push

---
+++ RestSharpTest/RestSharpTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07178c00-92cd-4c63-b90b-549a5608a881")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpTest/RestSharpTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using RestSharpTest.Model;
+using System;
+using System.Collections.Generic;
+
+namespace RestSharpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string baseURL = "https://reqres.in/api/";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new RestClient(baseURL);
+            var request = new RestRequest("data/{id}", Method.GET);
+            request.AddUrlSegment("id", 2);
+            var response = client.Execute<Data>(request);
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            string name = response.Data.color;
+
+        }
+    }
+}
